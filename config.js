1aebada56701c405d05eab361a50bce8e9bdd50a
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-15 00:42:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0x327DBE0D7653Cb6322D9A2BB92dBEe51Bf04B59F",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/noughtpad_rh",
   NAME:    "Noughtpad",
   TICKER:  "$NOUGHT",
